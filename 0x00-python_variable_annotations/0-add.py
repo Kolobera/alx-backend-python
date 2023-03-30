92542767caf345be7111916061f71baab03558cc
Ousmane Konate <konatealim@gmail.com>
2023-03-31 00:22:18 +0200

    Implementing add function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
-def add(a: float,b: float) -> float:
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
+def add(a: float, b: float) -> float:
     """Returns the sum of two floats"""
     return a + b
