1a9154da8225d893373a94ed25f7996f14e1c789
Ousmane Konate <konatealim@gmail.com>
2023-03-31 00:42:47 +0200

    Implementing sum functions

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python3
+sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
 
-a = __import__('4-define_variables').a
-pi = __import__('4-define_variables').pi
-i_understand_annotations = __import__('4-define_variables').i_understand_annotations
-school = __import__('4-define_variables').school
-
-print("a is a {} with a value of {}".format(type(a), a))
-print("pi is a {} with a value of {}".format(type(pi), pi))
-print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
-print("school is a {} with a value of {}".format(type(school), school))
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
