9452bc956f2654fa9eba911e5e9289620eb7e77f
Ousmane Konate <konatealim@gmail.com>
2023-04-04 15:27:40 +0200

    Implementing wait_n function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,13 @@
 '''Task 1's module.
 '''
 import asyncio
+from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Returns the list of all the delays (float values).
     '''
     delays = []
