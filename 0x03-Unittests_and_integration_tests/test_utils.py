c81f2c0d7db2ddf2cc8d2863fb46f353827e957f
Ousmane Konate <konatealim@gmail.com>
2023-04-25 00:21:16 +0200

    add task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,7 +67,11 @@ class TestMemoize(unittest.TestCase):
                 '''A property.
                 '''
                 return self.a_method()
-        with patch.object(TestClass, 'a_method') as mock_method:
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value=42
+            ) as mock_method:
             test_class = TestClass()
             test_class.a_property
             test_class.a_property
